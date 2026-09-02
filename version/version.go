bd6974b12fcf68d1e94939b7b7e1ce85ea29a8be
Guy Molinari <molinariguy@gmail.com>
2026-09-02 14:33:31 -0600

    Set version to 0.1.1-rc5

--- version/version.go
+++ version/version.go
@@ -26,7 +26,7 @@ const (
 // the current release version and omit commit/build-date metadata unless the
 // caller supplies ldflags.
 var (
-	Version   = "0.1.1-rc4"
+	Version   = "0.1.1-rc5"
 	Commit    = ""
 	BuildDate = ""
 )
